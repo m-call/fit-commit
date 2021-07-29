b6ff4d5ef23ebcb3f7c2cfaf243d1605a81e0978
Michael Callahan <mcallahan0@yahoo.com>
2021-07-29 13:22:23 -0400

    Created a post request for workoutExerciseRoutes.js

--- controllers/api/workoutExerciseRoutes.js
+++ controllers/api/workoutExerciseRoutes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const { WorkoutExercise } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const workoutExerciseData = await WorkoutExercise.create({
+            ...req.body,
+        });
+        res.status(200).json(workoutExerciseData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
