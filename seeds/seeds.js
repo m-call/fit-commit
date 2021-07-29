c29e1a069602612825cb117ddd47ed1d006bb0ce
csbonang <csgbonang@gmail.com>
2021-07-29 08:27:39 -0700

    completed seed.js file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,2 +1,35 @@
-// TODO 
-// Will do once models is completed 
+
+const sequelize = require('../config/connection');
+const { Exercises, Scores, Users, WorkoutExercises, WorkoutHistory} = require('../models');
+
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await Users.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const exercises = await Exercises.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    
+    const scores = await Scores.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    
+    const workoutExercises = await WorkoutExercises.bulkCreate(userData,{
+        individualHooks: true,
+        returning: true,
+    }); 
+
+    const workoutHistory = await WorkoutHistory.bulkCreate(userData,{
+        individualHooks: true,
+        returning: true,
+    }); 
+    process.exit(0); 
+}; 
+seedDatabase(); 
