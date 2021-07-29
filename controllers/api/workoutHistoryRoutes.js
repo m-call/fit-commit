ba34f99c52eb23f090a9944b73fd9506a48ce365
Zachary Khan <zacharythekhan@gmail.com>
2021-07-29 12:34:16 -0500

    workouthistory post handling

--- controllers/api/workoutHistoryRoutes.js
+++ controllers/api/workoutHistoryRoutes.js
@@ -1,2 +1,17 @@
 const router = require('express').Router();
 const { WorkoutHistory } = require('../../models');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newWorkout = await WorkoutHistory.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newWorkout);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
