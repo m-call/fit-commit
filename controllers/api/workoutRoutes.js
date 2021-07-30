a20f9a08bc100b0328842a5a6f88033c8ef4d780
Zachary Khan <zacharythekhan@gmail.com>
2021-07-30 11:55:32 -0500

    workout routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
-const { WorkoutHistory } = require('../../models');
+const { Workout, WorkoutExercise, Exercise } = require('../../models');
 
+// Get all workouts
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const workoutData = await Workout.findAll();
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// Get one workout
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const workoutData = await Workout.findByPk(req.params.id, {
+      include: [{ model: Exercise, through: WorkoutExercise, as: 'workout_exercise'}]
+    })
+  }
+})
+
+// Add a workout
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newWorkout = await WorkoutHistory.create({
+    const newWorkout = await Workout.create({
       ...req.body,
       user_id: req.session.user_id,
     });
@@ -16,10 +36,13 @@ router.post('/', withAuth, async (req, res) => {
 
 module.exports = router;
 
+// Update a workout
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    const workoutData = await WorkoutHistory.findByPk({});
+    const workoutData = await Workout.findByPk({});
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+// Delete a workout
