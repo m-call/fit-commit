497ee4b9a5a33c7b8ff94b78c47ef291a48a0721
csbonang <csgbonang@gmail.com>
2021-08-02 19:34:07 -0700

    updated final

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
 const { User, Score } = require('../../models');
+const bcrypt = require('bcrypt'); 
 
+router.post('/signup', async (req, res) => {
+  try {
+    console.log('THIS IS INSIDE OF SIGNUP', req.body); 
+    const userData = await User.create({
+      username: req.body.username,
+    password: req.body.password});
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.logged_in = true;
+      console.log('do I save?')
+      res.json(userData);
+    }); 
+  }
+  catch (err) {
+  res.status(400).json(err);
+}
+});
 router.get('/:id', async (req, res) => {
   try {
       const userData = await User.findOne({
@@ -41,7 +60,10 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    // const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await bcrypt.compareSync(req.body.password, userData.password);
+    console.log("========================="); 
+    console.log(validPassword); 
 
     if (!validPassword) {
       res
@@ -53,7 +75,6 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
       res.json({ user: userData, message: 'You are now logged in!' });
     });
   } catch (err) {
